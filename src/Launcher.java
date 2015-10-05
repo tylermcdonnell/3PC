422957f77275b074646fcac3223b3888ed443f7a
mikefeilbach <mikefeilbach@gmail.com>
2015-10-05 17:08:14 -0500

    Fixed small bug in Playlist--it is tested now

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,9 @@
+import playlist.Playlist;;
+
 
 public class Launcher {
 
-	public static void main(String args[])
+	public static void main(String args[]) throws Exception
 	{
 		// TODO: Mike
 		// Parse config file (which will be in 3PC/config.txt) and
