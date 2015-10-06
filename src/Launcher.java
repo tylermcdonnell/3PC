32375cb493a6395150e4b02159164dcc1356c527
mikefeilbach <mikefeilbach@gmail.com>
2015-10-06 12:47:39 -0500

    Communication layer added

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,24 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import framework.Config;
+import framework.NetController;
 
 public class Launcher {
 
 	// A list of all thread handles.
 	public static ArrayList<Thread> threads = new ArrayList<Thread>();
 	
+	// A list of all net controllers (corresponding to the same element in
+	// the threads list).
+	public static ArrayList<NetController> netControllers = new ArrayList<NetController>();
+	
+	//  Configure commands.
+	private static final String ADD_CMD = "add";
+	private static final String REMOVE_CMD = "remove";
+	private static final String EDIT_CMD = "edit";
+	private static final String CREATE_PROCESSES_CMD = "cp";
+	private static final String KILL_CMD = "kill";
+	private static final String KILL_ALL_CMD = "killAll";
+	private static final String KILL_LEADER_CMD = "killLeader";
+	private static final String REVIVE_CMD = "revive";
+	private static final String REVIVE_LAST_CMD = "reviveLast";
+	private static final String REVIVE_ALL_CMD = "reviveAll";
+	private static final String PARTIAL_MESSAGE_CMD = "pm";
+	private static final String RESUME_MESSAGES_CMD = "rm";
+	private static final String ALL_CLEAR_CMD = "allClear";
+	private static final String REJECT_NEXT_CHANGE_CMD = "rejectNextChange";
+	private static final String EXIT_CMD = "e";
+	
+	// Number of processes we choose to create for this execution.
+	public static int numProcesses;
 	
 	public static void main(String args[]) throws Exception
 	{
-		// TODO: Mike
-		// Parse config file (which will be in 3PC/config.txt) and
-		// then loop while file hasNextLine() to execute all
-		// commands.
-		
 		System.out.println("Input commands to control 3PC flow:");
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		while(true)
+		while (true)
 		{
 			String command = scanner.nextLine();
 			
@@ -26,83 +50,181 @@ public class Launcher {
 			String[] tokens = command.split(" ");
 			String firstToken = tokens[0];
 			
-			if (firstToken.equals("add")) {
+			if (firstToken.equals(ADD_CMD)) {
 				
 				System.out.println("Adding <" + tokens[1] + ", " + tokens[2] + ">.");
 				
-			} else if (firstToken.equals("remove")) {
+			} else if (firstToken.equals(REMOVE_CMD)) {
 				
 				System.out.println("Removing <" + tokens[1] + ">.");
 			
-			} else if (firstToken.equals("edit")) {
+			} else if (firstToken.equals(EDIT_CMD)) {
 				
 				System.out.println("Editting <" + tokens[1] + ", ?> to  <" + tokens[2] + ", " + tokens[3] + ">.");
 				
-			} else if (firstToken.equals("createProcesses")) {
+			} else if (firstToken.equals(CREATE_PROCESSES_CMD)) {
 				
 				System.out.println("Creating " + tokens[1] + " \"processes.\"");
 				createProcesses(tokens[1]);
 				
-			} else if (firstToken.equals("kill")) {
+			} else if (firstToken.equals(KILL_CMD)) {
+				
+				// Note: kill should spin-wait on the status of the thread to be
+				// terminated
 				
 				System.out.println("Killing process " + tokens[1] + ".");
 				
-			} else if (firstToken.equals("killAll")) {
+			} else if (firstToken.equals(KILL_ALL_CMD)) {
+				
+				// This would be a total failure? this isn't allowed?
 				
 				System.out.println("Killing all processes.");
 				
-			} else if (firstToken.equals("killLeader")) {
+			} else if (firstToken.equals(KILL_LEADER_CMD)) {
 				
 				System.out.println("Killing the leader.");
 				
-			} else if (firstToken.equals("revive")) {
+			} else if (firstToken.equals(REVIVE_CMD)) {
 				
 				System.out.println("Reviving process " + tokens[1] + ".");
 				
-			} else if (firstToken.equals("reviveLast")) {
+			} else if (firstToken.equals(REVIVE_LAST_CMD)) {
 				
 				System.out.println("Reviving last killed process.");
 				
-			} else if (firstToken.equals("reviveAll")) {
+			} else if (firstToken.equals(REVIVE_ALL_CMD)) {
 				
 				System.out.println("Reviving all processes.");
 				
-			} else if (firstToken.equals("partialMessage")) {
+			} else if (firstToken.equals(PARTIAL_MESSAGE_CMD)) {
 				
 				System.out.println("Process " + tokens[1] + " will send " + tokens[2] + " messages then stop.");
 				
-			} else if (firstToken.equals("resumeMessages")) {
+			} else if (firstToken.equals(RESUME_MESSAGES_CMD)) {
 				
 				System.out.println("Process " + tokens[1] + " will resume sending messages.");
 				
-			} else if (firstToken.equals("allClear")) {
+			} else if (firstToken.equals(ALL_CLEAR_CMD)) {
 				
 				System.out.println("allClear was called.");
 				
-			} else if (firstToken.equals("rejectNextChange")) {
+			} else if (firstToken.equals(REJECT_NEXT_CHANGE_CMD)) {
 				
 				System.out.println("Process " + tokens[1] + " will reject next change.");
 				
-			} else {
+			} else if (firstToken.equals(EXIT_CMD)) {
 				
-				System.out.println("Unrecognized command. Program terminating.");
+				System.out.println("Exting after closing all net controllers.");
+				
+				for (int i = 0; i < netControllers.size(); i++) {
+					netControllers.get(i).shutdown();
+				}
+				
+				System.exit(-1);
+			}
+			
+			else {
+				
+				System.out.println("Unrecognized command. Closing all net controllers. Program terminating.");
+				
+				for (int i = 0; i < netControllers.size(); i++) {
+					netControllers.get(i).shutdown();
+				}
+				
 				System.exit(-1);
 			}
 		}
 	}
 	
+	
+	/**
+	 * Creates the specified number of "processes" (threads). For each
+	 * thread created, add its handle to a list.
+	 * 
+	 * @param numProcesses, the specified number of "processes" to create.
+	 */
 	private static void createProcesses(String numProcesses) {
 		
 		int numProcs = Integer.valueOf(numProcesses);
 		
+		// Store the number of processes to a global variable.
+		Launcher.numProcesses = numProcs;
+		
 		for (int i = 0; i < numProcs; i++) {
 			
-			Runnable r = new MikeProcess(String.valueOf(i));
+			NetController nc = createNetController(i);
+			
+			// Pass in "i" as the process number for this process.
+			Runnable r = new MikeProcess(i, nc);
 			Thread d = new Thread(r);
 			d.start();
 			
+			// Add to our list of threads.
 			threads.add(d);
 		}
+	}
+	
+	
+	/**
+	 * Creates a NetController for the given process, described by its process
+	 * number.
+	 * 
+	 * @param processNumber, this process' process number.
+	 * 
+	 * @return a NetController for the given process, described by its process
+	 * number.
+	 */
+	private static NetController createNetController(int processNumber) {
+		
+		//**********************************************************************
+		//* Setup communication for this process with all other processes.
+		//**********************************************************************
+		NetController nc = null;
+		
+		// Dynamically create a config file for this process.
+		// Reuse the same file for all processes.
+		String fileName = "config" + processNumber + ".txt";
+		File file = new File(fileName);
+		
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// NumProcesses field.
+		out.println("NumProcesses=" + Launcher.numProcesses);
+		
+		// ProcNum field.
+		out.println("ProcNum=" + processNumber);
+		
+		// host fields.
+		for (int i = 0; i < Launcher.numProcesses; i++) {
+			out.println("host" + i + "=localhost");
+		}
+		
+		// port fields.
+		for (int i = 0; i < Launcher.numProcesses; i++) {
+			out.println("port" + i + "=" + (6100 + i));
+		}
+		
+		out.flush();
+		out.close();
+		
+		try {
+			
+			Config config = new Config(fileName);
+			nc = new NetController(config);
+			
+		} catch (Exception e) {	
+			e.printStackTrace();
+		}
+		
+		// Add this to the list of net controllers in the entire system.
+		netControllers.add(nc);
+		
+		return nc;
 	}
 }
 
--- src/MikeProcess.java
+++ src/MikeProcess.java
@@ -1,20 +1,75 @@
+import java.util.ArrayList;
 
+import framework.NetController;
+
+/**
+ * Each "process" will continuously run code in the "run" method to implement
+ * 3PC.
+ * 
+ * @author Mike Feilbach
+ * 
+ */
 public class MikeProcess implements Runnable {
 	
+	// This process' number, as assigned in the code -- 0 for the first
+	// created thread, 1 for the second created thread, etc.
+	private int processNumber;
 	
-	// This process' ID.
+	// This process' ID, as assigned by the scheduler -- this may mean
+	// that the first created process is 11, the second created process
+	// is 12, etc.
 	private long processID;
 	
-	public MikeProcess(Object args) {
+	// This process' net controller.
+	private NetController nc;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param processNumber, the assigned number for this "process."
+	 */
+	public MikeProcess(int processNumber, NetController nc) {
+		
+		this.processNumber = processNumber;
+		this.nc = nc;
 	}
 	
+	/**
+	 * Each "process" will run this code for the span of its life.
+	 */
 	public void run() {
 		
+		// Store this process' ID.
 		this.processID = Thread.currentThread().getId();
+		System.out.println("Process " + this.processID + " is up, processNumber: " + this.processNumber);
 		
-		System.out.println("Process " + this.processID + " is up!");
-	
-		// Wait for message from coord.
-		// How to handle timeout?
+		
+		// Testing. This can be used to kill threads.
+		if (Thread.interrupted()) {
+	        System.out.println("INTERRUPTED");
+	        return;
+	    }
+		
+		// Send a message from p0 to p1.
+		if (this.processNumber == 0) {
+			nc.sendMsg(1, "HI THERE from p_0");
+		} else if (this.processNumber == 1) {
+			nc.sendMsg(0,  "HI THERE FROM p_1");
+		}
+		
+		
+		// Wait for a message.
+		ArrayList<String> messages = new ArrayList<String>();
+			
+		while (messages.size() == 0) {
+			messages = (ArrayList<String>)nc.getReceivedMsgs();
+		}
+			
+		 // Got a message.
+		for (int i = 0; i < messages.size(); i++) {
+			System.out.println("Message: " + messages.get(i));
+		}
+		
+		nc.shutdown();
 	}
 }
