cb617a1a2b718923b6d9f3251bda6360f7164e15
mikefeilbach <mikefeilbach@gmail.com>
2015-10-08 09:46:56 -0500

    Committing just in case we need some old code

--- src/MikeProcess.java
+++ src/MikeProcess.java
@@ -11,6 +11,8 @@ import framework.NetController;
  */
 public class MikeProcess implements Runnable {
 	
+	private boolean isCoordinator;
+	
 	// This process' number, as assigned in the code -- 0 for the first
 	// created thread, 1 for the second created thread, etc.
 	private int processNumber;
@@ -32,6 +34,12 @@ public class MikeProcess implements Runnable {
 		
 		this.processNumber = processNumber;
 		this.nc = nc;
+		
+		if (processNumber == 0) {
+			this.isCoordinator = true;
+		} else {
+			this.isCoordinator = false;
+		}
 	}
 	
 	/**
@@ -41,15 +49,88 @@ public class MikeProcess implements Runnable {
 		
 		// Store this process' ID.
 		this.processID = Thread.currentThread().getId();
-		System.out.println("Process " + this.processID + " is up, processNumber: " + this.processNumber);
+		println("Process " + this.processID + " is up, processNumber: " + this.processNumber);
+		
+		//**********************************************************************
+		//* Step 1: if coordinator, send VOTE-REQs to all participants.
+		//*         if participant, receive one VOTE-REQ.
+		//**********************************************************************
+		if (this.isCoordinator) {
+			
+			// Wait for other threads to be created (coordinator is created first).
+			sleep(1);
+			
+			println(this.processNumber + ": I am coord, sending VOTE-REQs to all participants");
+			sendMessageToAllOthers("VOTE-REQ");
+			
+		} else {
+			
+			// If participant, receive a single VOTE-REQ message.
+			ArrayList<String> messages = waitForNumMessages(1);
+			
+			println(this.processNumber + ": received: " + messages.get(0));
+		}
+		
+		//**********************************************************************
+		//* Step 2: if participant, send YES or NO vote to coordinator.
+		//*         if coordinator, receive votes from all participants.
+		//**********************************************************************
+		boolean allVotesYes = false;
+		
+		if (this.isCoordinator) {
+			
+			// Wait for messages from all participants.
+			ArrayList<String> messages = waitForNumMessages(Launcher.numProcesses - 1);
+			System.out.println("Received all votes from parts");
+			
+			// Check on the votes -- see if they're all "YES."
+			for (int i = 0; i < messages.size(); i++) {
+				
+				if (messages.get(i).equals("NO")) {
+					allVotesYes = false;
+				}
+			}
+		} else {
+			
+			// If participant, send vote (send YES for now).
+			nc.sendMsg(0, "YES");
+			println("Sent YES from p " + this.processNumber);
+		}
+		
+		//***********************************************************************
+		//* Step 3: if coordinator, if all votes are yes, send PRE-COMMMITS.
+		//*         if participant and voted yes, wait for PRE-COMMIT.
+		//**********************************************************************
+		if (this.isCoordinator) {
+			
+			// Coordinator.
+			if (allVotesYes) {
+				
+				println(this.processNumber + ": I am coord, sending PRE-COMMITs to all parts");
+				for (int i = 0; i < Launcher.numProcesses; i++) {
+					if (i == this.processNumber) {
+						// Do not send message to yourself.
+					} else {
+						nc.sendMsg(i, "PRE-COMMIT");
+					}
+				}
+			}
+			
+			
+		} else {
+			// Participant code.
+		}
+		
+		if (this.isCoordinator) {
+			println("Coord done");
+		} else {
+			println("Part done");
+		}
 		
 		
-		// Testing. This can be used to kill threads.
-		if (Thread.interrupted()) {
-	        System.out.println("INTERRUPTED");
-	        return;
-	    }
+		nc.shutdown();
 		
+		/*
 		// Send a message from p0 to p1.
 		if (this.processNumber == 0) {
 			nc.sendMsg(1, "HI THERE from p_0");
@@ -59,17 +140,114 @@ public class MikeProcess implements Runnable {
 		
 		
 		// Wait for a message.
-		ArrayList<String> messages = new ArrayList<String>();
+		
 			
-		while (messages.size() == 0) {
-			messages = (ArrayList<String>)nc.getReceivedMsgs();
-		}
+		
 			
 		 // Got a message.
 		for (int i = 0; i < messages.size(); i++) {
-			System.out.println("Message: " + messages.get(i));
+			println("Message: " + messages.get(i));
+		}
+		*/
+		
+		//nc.shutdown();
+		
+		/*
+		// Testing. This can be used to kill threads.
+		if (Thread.interrupted()) {
+	       println("INTERRUPTED");
+	        return;
+	    }
+	    */
+	}
+	
+	/**
+	 * Sends a message to all other processes in the system.
+	 * 
+	 * @param s, the message to send.
+	 */
+	private void sendMessageToAllOthers(String s) {
+		
+		for (int i = 0; i < Launcher.numProcesses; i++) {
+			if (i == this.processNumber) {
+				
+			} else {
+				nc.sendMsg(i, s);
+			}
+		}
+	}
+	
+	/**
+	 * Wait for the specified number of messages to come in. When they do,
+	 * return the messages. NOTE: this function is blocking, and will wait
+	 * indefinitely for the correct number of messages to come in.
+	 * 
+	 * 
+	 * @param numMessages, the specified number of messages.
+	 * 
+	 * @return the received messages.
+	 */
+	private ArrayList<String> waitForNumMessages(int numMessages) {
+		
+		ArrayList<String> messages = new ArrayList<String>();
+		ArrayList<String> receivedMsgs = new ArrayList<String>();
+		
+		while (true) {
+			
+			// See how many messages are waiting right now.
+			messages = (ArrayList<String>)nc.getReceivedMsgs();
+			
+			// Exhaust the messages that were received into the
+			// return list.
+			for (int i = 0; i < messages.size(); i++) {
+				
+				receivedMsgs.add(messages.get(i));
+			}
+			
+			// If we've gotten all the messages we need, exit
+			// this loop and return the messages
+			if (receivedMsgs.size() == numMessages) {
+				break;
+			}
 		}
 		
-		nc.shutdown();
+		return receivedMsgs;
+	}
+	
+	
+	/**
+	 * Implementation of System.out.println with mutual exclusion (only one
+	 * thread can be printing at once, and will finish printing its contents
+	 * before another thread can acquire the lock.
+	 * 
+	 * @param s, the String to print.
+	 */
+	private static void println(String s) {
+		
+		try {
+			Launcher.printSem.acquire();
+		} catch (InterruptedException e) {
+			System.out.println("Could not acquire print semaphore:");
+			e.printStackTrace();
+		}
+		
+		System.out.println(s);
+		
+		Launcher.printSem.release();
+	}
+	
+	/**
+	 * Sleep for the given number of seconds.
+	 * 
+	 * @param numSeconds, the given number of seconds.
+	 */
+	private void sleep(int numSeconds) {
+		
+		try {
+			Thread.sleep(numSeconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
